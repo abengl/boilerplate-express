252d848c2cefcf18e7db9628c43f5037e0330717
abengl <alebendigl@gmail.com>
2023-07-10 12:20:26 -0500

    Implementing Express route part 1-6

--- myApp.js
+++ myApp.js
@@ -1,39 +1,23 @@
-let express = require('express');
+require("dotenv").config();
+let express = require("express");
 let app = express();
 
+/* app.use("/public", express.static(__dirname + "/public"));
 
+app.get("/", (req, res) => {
+  // res.send("Hello Express");
+  res.sendFile(__dirname + "/views/index.html");
+});
+ */
+app.get("/json", (req, res) => {
+  const response = {
+    message: "Hello json",
+  };
 
+  if (process.env.MESSAGE_STYLE == "uppercase") {
+    response.message = "HELLO JSON";
+  }
+  res.json(response);
+});
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
- module.exports = app;
+module.exports = app;
