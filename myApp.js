1706b78afd5b7e801defb26391eff22aa554e9c9
abengl <alebendigl@gmail.com>
2023-07-12 15:32:25 -0500

    Tasks from 1-9

--- myApp.js
+++ myApp.js
@@ -1,6 +1,53 @@
-require("dotenv").config();
+//require("dotenv").config();
 let express = require("express");
 let app = express();
+
+app.get("/name", (req, res) => {
+  let firstName = req.query.first;
+  let lastName = req.query.last;
+  // OR you can destructure and rename the keys
+  //var { first: firstName, last: lastName } = req.query;
+  res.json({ name: `${firstName} ${lastName}` });
+});
+
+app.use(express.static(__dirname + "/public"));
+
+/* app.get("/:word/echo", (req, res) => {
+  const { word } = req.params;
+  res.json({ echo: word });
+});
+
+app.use(express.static(__dirname + "/public"));
+ */
+/* app.get(
+  "/now",
+  (req, res, next) => {
+    req.time = new Date().toString();
+    next();
+  },
+  (req, res) => {
+    res.send({ time: req.time });
+  }
+);
+ */
+//Alternative to save the middleware for further uses
+/* const middleware = (req, res, next) => {
+  req.time = new Date().toString();
+  next();
+};
+
+app.get("/now", middleware, (req, res) => {
+  res.send({
+    time: req.time
+  });
+}); */
+
+/* app.use((req, res, next) => {
+  console.log(`${req.method} ${req.path} - ${req.ip}`);
+  next();
+});
+
+app.use(express.static(__dirname + "/public")); */
 
 /* app.use("/public", express.static(__dirname + "/public"));
 
@@ -9,7 +56,8 @@ app.get("/", (req, res) => {
   res.sendFile(__dirname + "/views/index.html");
 });
  */
-app.get("/json", (req, res) => {
+
+/* app.get("/json", (req, res) => {
   const response = {
     message: "Hello json",
   };
@@ -19,5 +67,5 @@ app.get("/json", (req, res) => {
   }
   res.json(response);
 });
-
+*/
 module.exports = app;
